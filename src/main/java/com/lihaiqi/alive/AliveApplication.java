b9a3a97f13e1bb8fdfc23eb51cf40f44d95b26a4
lihaiqi <lihaiqi@keyto168.com>
2026-01-12 18:05:58 +0800

    创建项目info

---
+++ src/main/java/com/lihaiqi/alive/AliveApplication.java
@@ -0,0 +1,13 @@
+package com.lihaiqi.alive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lihaiqi/alive/AliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lihaiqi.alive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
